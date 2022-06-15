4d4255bb436e323c4cdcafc6e8e7fc4dcc05ade7
bhavya2921 <bhavyagra3008@gmail.com>
2022-06-15 20:49:10 +0530

    Verification page created

---
+++ app/src/main/java/com/example/ideation/EmailVerificationActivity.java
@@ -0,0 +1,24 @@
+package com.example.ideation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class EmailVerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_verification);
+    }
+
+    public void changeEmail(View view) {
+    }
+
+    public void resendEmail(View view) {
+    }
+
+    public void check(View view) {
+    }
+}
