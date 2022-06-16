5642f04ebb38ca8272b7ccbb300d8cb8d65fb72c
bhavya2921 <bhavyagra3008@gmail.com>
2022-06-16 15:33:07 +0530

    user model class created and changes done

--- app/src/main/java/com/example/ideation/BottomNavActivity.java
+++ app/src/main/java/com/example/ideation/BottomNavActivity.java
@@ -1,14 +1,49 @@
 package com.example.ideation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.ideation.Fragments.BookmarkFragment;
+import com.example.ideation.Fragments.ExploreFragment;
+import com.example.ideation.Fragments.FeedFragment;
+import com.example.ideation.Fragments.HomeFragment;
+import com.example.ideation.Fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class BottomNavActivity extends AppCompatActivity {
+
+    BottomNavigationView bottom_nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_nav);
+        replace(new HomeFragment());
+
+        bottom_nav = findViewById(R.id.nav);
+
+        bottom_nav.setItemHorizontalTranslationEnabled(true);
+        bottom_nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId()==R.id.home) replace(new HomeFragment());
+                else if (item.getItemId()==R.id.feed) replace(new FeedFragment());
+                else if (item.getItemId()==R.id.bookmark) replace(new BookmarkFragment());
+                else if (item.getItemId()==R.id.idea_about) replace(new ExploreFragment());
+                else replace(new ProfileFragment());
+                return true;
+            }
+        });
+
+    }
+
+    private void replace(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().
+                replace(R.id.frame_layout,fragment).commit();
     }
 }
---
+++ app/src/main/java/com/example/ideation/Model/UserModel.java
@@ -0,0 +1,81 @@
+package com.example.ideation.Model;
+
+import java.io.Serializable;
+
+public class UserModel implements Serializable {
+    private String userName;
+    private String profession;
+    private String userID;
+    private String imageURL;
+    private String bio;
+    private String Address;
+    private int followCount;
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public UserModel() { }
+
+    public int getFollowCount() {
+        return followCount;
+    }
+
+    public void setFollowCount(int followCount) {
+        this.followCount = followCount;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+
+    public UserModel(String userName, String profession, String userID, String address, String imageURL) {
+        this.userName = userName;
+        this.profession = profession;
+        this.userID = userID;
+        this.followCount=0;
+        this.Address=address;
+        this.imageURL=imageURL;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+}
