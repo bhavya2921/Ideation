0babb14fca18cadd8b495b70375e44279a779703
bhavya2921 <bhavyagra3008@gmail.com>
2022-06-17 13:49:37 +0530

    home java code done

--- app/src/main/java/com/example/ideation/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/ideation/Fragments/HomeFragment.java
@@ -1,22 +1,75 @@
 package com.example.ideation.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.ideation.Adapter.PostAdapter;
+import com.example.ideation.AddPostActivity;
+import com.example.ideation.Model.PostModel;
 import com.example.ideation.R;
+import com.example.ideation.databinding.FragmentHomeBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
 
+    FragmentHomeBinding binding;
+    private PostAdapter adapter;
+    private ArrayList<PostModel> posts = new ArrayList<PostModel>();
+    FirebaseFirestore fstore;
+    FirebaseDatabase db;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        binding=FragmentHomeBinding.inflate(inflater, container, false);
+        fstore = FirebaseFirestore.getInstance();
+        adapter = new PostAdapter(posts,getContext(),false);
+        db = FirebaseDatabase.getInstance();
+
+        db.getReference().child("posts").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot shot: snapshot.getChildren()){
+                    PostModel post = shot.getValue(PostModel.class);
+                    posts.add(post);
+                }
+                adapter.notifyDataSetChanged();
+                binding.progressBar3.setVisibility(View.GONE);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        binding.recyclerView.setHasFixedSize(true);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.recyclerView.setAdapter(adapter);
+
+
+
+        binding.createPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(), AddPostActivity.class));
+            }
+        });
+        return binding.getRoot();
     }
 }
