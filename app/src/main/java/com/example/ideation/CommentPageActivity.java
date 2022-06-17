81869a691537f9fdd6927a8f9e0e8be773a746e2
bhavya2921 <bhavyagra3008@gmail.com>
2022-06-17 17:03:18 +0530

    profile fragment one changes done

--- app/src/main/java/com/example/ideation/CommentPageActivity.java
+++ app/src/main/java/com/example/ideation/CommentPageActivity.java
@@ -1,14 +1,131 @@
 package com.example.ideation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.ideation.Adapter.CommentAdapter;
+import com.example.ideation.Model.CommentModel;
+import com.example.ideation.Model.UserModel;
+import com.example.ideation.databinding.ActivityCommentPageBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CommentPageActivity extends AppCompatActivity {
+
+    ActivityCommentPageBinding binding;
+    private String postID,authorID;
+    Intent intent;
+    FirebaseUser fuser;
+    ArrayList<CommentModel> commentList = new ArrayList<>();
+    CommentAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_comment_page);
+        binding = ActivityCommentPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        intent = getIntent();
+        postID = intent.getStringExtra("postId");
+        authorID = intent.getStringExtra("authorId");
+        fuser = FirebaseAuth.getInstance().getCurrentUser();
+
+        getUserImage();
+
+
+        adapter = new CommentAdapter(CommentPageActivity.this,commentList);
+        binding.commentsRecycler.setHasFixedSize(true);
+        binding.commentsRecycler.setLayoutManager(new LinearLayoutManager(CommentPageActivity.this));
+        binding.commentsRecycler.setAdapter(adapter);
+
+        getComments();
+
+        binding.postComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String comment = binding.commentText.getText().toString().trim();
+                if (!comment.isEmpty()){
+                    pushComment(comment);
+                }
+            }
+        });
+
+    }
+
+    private void getComments() {
+        FirebaseDatabase.getInstance().getReference().child("comments").child(postID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                commentList.clear();
+                for(DataSnapshot shot : snapshot.getChildren()){
+                    CommentModel comment = shot.getValue(CommentModel.class);
+                    commentList.add(comment);
+                }
+                adapter.notifyDataSetChanged();
+                binding.progressBar6.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+    private void pushComment(String comment) {
+        HashMap<String,Object> map = new HashMap<>();
+
+        map.put("comment",comment);
+        map.put("publisher",fuser.getUid());
+
+        FirebaseDatabase.getInstance().getReference().child("comments").child(postID).push().setValue(map)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            makeToast("Comment Posted");
+                            binding.commentText.setText("");
+                        }
+                        else makeToast(task.getException().getMessage());
+                    }
+                });
+    }
+
+    private void makeToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void getUserImage() {
+        FirebaseDatabase.getInstance().getReference().child("Users").child(fuser.getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        UserModel user = snapshot.getValue(UserModel.class);
+                        if (!user.getImageURL().equals("default"))
+                            Picasso.get().load(user.getImageURL()).into(binding.userProfile);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/ideation/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/ideation/Fragments/ProfileFragment.java
@@ -1,21 +1,176 @@
 package com.example.ideation.Fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.ideation.Model.PostModel;
+import com.example.ideation.Model.UserModel;
 import com.example.ideation.R;
+import com.example.ideation.RegisterActivity;
+import com.example.ideation.databinding.FragmentProfileBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class ProfileFragment extends Fragment {
+
+    FragmentProfileBinding binding;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fstore;
+    FirebaseDatabase database;
+    FirebaseStorage storage;
+    ActivityResultLauncher<String> launcher;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        binding=FragmentProfileBinding.
+                inflate(inflater, container, false);
+        fAuth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+        database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+
+        getFollowCount();
+        getFollowingCount();
+        getPostCount();
+
+        launcher = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri uri) {
+                binding.userProfileImage.setImageURI(uri);
+                final StorageReference reference = storage.getReference().child("image");
+                reference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(getContext(), "Uploded", Toast.LENGTH_SHORT).show();
+                        reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                database.getReference().child("Users").child(fAuth.getCurrentUser().
+                                        getUid()).child("imageURL").setValue(uri.toString());
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+        getUser();
+
+        binding.changeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launcher.launch("image/*");
+            }
+        });
+
+        binding.logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fAuth.signOut();
+                startActivity(new Intent(getContext(), RegisterActivity.class));
+                getActivity().finish();
+            }
+        });
+
+        return binding.getRoot();
     }
+
+    private void getPostCount() {
+        database.getReference().child("posts").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                int cont=0;
+                for (DataSnapshot shot : snapshot.getChildren()){
+                    PostModel post = shot.getValue(PostModel.class);
+                    if (post.getUserID().equals(fAuth.getCurrentUser().getUid())) cont++;
+                }
+                binding.postCount.setText(cont+"posts");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void getFollowCount() {
+        database.getReference().child("follow").child(fAuth.getCurrentUser().getUid()).child("followers")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        binding.followCount.setText(snapshot.getChildrenCount()+"");
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+    }
+
+    private void getFollowingCount() {
+        database.getReference().child("follow").child(fAuth.getCurrentUser().getUid()).child("following")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        binding.followingCount.setText(snapshot.getChildrenCount()+" following");
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+    }
+
+
+
+    private void getUser() {
+        String userID= fAuth.getCurrentUser().getUid();
+        database.getReference().child("Users").child(userID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                UserModel user = snapshot.getValue(UserModel.class);
+                if (user!=null) updateUser(user);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void updateUser(UserModel user) {
+        binding.constraintLayout2.setVisibility(View.VISIBLE);
+        binding.progressBar4.setVisibility(View.GONE);
+        if (!user.getImageURL().equals("default")){
+            Picasso.get().load(user.getImageURL()).into(binding.userProfileImage);
+        }
+        binding.userName.setText(user.getUserName());
+        binding.workProfession.setText(user.getProfession());
+        binding.followCount.setText(String.valueOf(user.getFollowCount()));
+    }
+
 }
